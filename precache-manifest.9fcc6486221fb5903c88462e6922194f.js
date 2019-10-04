c12a125eaea981776431f08e04841d8fca1f589e
João Forja <joao.forja@challenge.pt>
2019-10-04 17:40:59 +0100

    Updates

---
+++ precache-manifest.9fcc6486221fb5903c88462e6922194f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca436387f6ec318a140ac2545aa1f9ad",
+    "url": "/GapminderClone/index.html"
+  },
+  {
+    "revision": "d6d403a106f37abf126f",
+    "url": "/GapminderClone/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5a51095092b2417c3b5e",
+    "url": "/GapminderClone/static/js/2.2572640c.chunk.js"
+  },
+  {
+    "revision": "d6d403a106f37abf126f",
+    "url": "/GapminderClone/static/js/main.9cbd14e3.chunk.js"
+  },
+  {
+    "revision": "251c7981d30b9df7f877",
+    "url": "/GapminderClone/static/js/runtime-main.6d6a7c5c.js"
+  }
+]);
